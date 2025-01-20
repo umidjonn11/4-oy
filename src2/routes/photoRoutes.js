d4c6a1b287bf3d5360d750bbf075cfbe82dfd4fe
Umedov Umidjon <lustrum062@gmail.com>
2025-01-20 13:20:56 +0300

    refreshments

--- src2/routes/photoRoutes.js
+++ src2/routes/photoRoutes.js
@@ -1,26 +1,20 @@
-// photoRoutes.js
 import express from 'express';
 import multer from 'multer';
 import { photoController } from '../controllers/photoController.js';
 
 const router = express.Router();
 
-// Set up Multer storage engine
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    // Define the folder where the files will be saved
-    cb(null, 'uploads'); // Make sure this folder exists
+    cb(null, 'uploads'); 
   },
   filename: function (req, file, cb) {
-    // Set the filename (we are using original filename here)
     cb(null, Date.now() + '-' + file.originalname);
   }
 });
 
-// Initialize Multer with the storage configuration
 const upload = multer({ storage: storage });
 
-// Define the route for uploading a single image
 router.post('/upload', upload.single('image'), photoController.create);
 
 export default router;
